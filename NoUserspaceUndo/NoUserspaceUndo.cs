3f10dea04f459400280d11746cb6a71be3231d8c
badhaloninja <badhaloninja@badhalo.ninja>
2022-03-10 23:15:15 -0600

    How did I miss that for a week

--- NoUserspaceUndo/NoUserspaceUndo.cs
+++ NoUserspaceUndo/NoUserspaceUndo.cs
@@ -1,3 +1,4 @@
+using FrooxEngine;
 using FrooxEngine.Undo;
 using HarmonyLib;
 using NeosModLoader;
@@ -8,7 +9,7 @@ namespace NoUserspaceUndo
     {
         public override string Name => "NoUserspaceUndo";
         public override string Author => "badhaloninja";
-        public override string Version => "1.1.0";
+        public override string Version => "1.1.1";
         public override string Link => "https://github.com/badhaloninja/NoUserspaceUndo";
 
         private static ModConfiguration config;
@@ -26,17 +27,23 @@ namespace NoUserspaceUndo
         [AutoRegisterConfigKey]
         private static readonly ModConfigurationKey<bool> AllowUserspaceUndo = new ModConfigurationKey<bool>("allowUserspaceUndo", "Allow using undo in userspace", () => false);
 
+
+        public static bool Check(UndoManager instance)
+        {
+            if (!instance.World.IsUserspace()) return true;
+            return config.GetValue(AllowUserspaceUndo);
+        }
         class UndoManager_Patch
         {
             [HarmonyPatch(typeof(UndoManager), "Undo")]
             class Undo_Patch
             {
-                public static bool Prefix() => config.GetValue<bool>(AllowUserspaceUndo);
+                public static bool Prefix(UndoManager __instance) => Check(__instance);
             }
             [HarmonyPatch(typeof(UndoManager), "Redo")]
             class Redo_Patch
             {
-                public static bool Prefix() => config.GetValue<bool>(AllowUserspaceUndo);
+                public static bool Prefix(UndoManager __instance) => Check(__instance);
             }
         }
     }
